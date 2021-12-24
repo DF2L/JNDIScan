3ee2d7ececfd744d7a65bc234f3efebf8dd53d38
EmYiQing <2023503307@qq.com>
2021-12-24 21:15:18 +0800

    rmi

--- core/server.go
+++ core/server.go
@@ -41,6 +41,7 @@ func acceptProcess(conn *net.Conn) {
 	}
 	hexStr := fmt.Sprintf("%x", buf[:num])
 	// LDAP Protocol
+	// https://ldap.com/ldapv3-wire-protocol-reference-bind
 	if "300c020101600702010304008000" == hexStr {
 		data := []byte{
 			0x30, 0x0c, 0x02, 0x01, 0x01, 0x61, 0x07,
@@ -67,10 +68,39 @@ func acceptProcess(conn *net.Conn) {
 	}
 	// RMI Protocol
 	if checkRMI(buf) {
+		data := []byte{
+			0x4e, 0x00, 0x09, 0x31, 0x32,
+			0x37, 0x2e, 0x30, 0x2e, 0x30,
+			0x2e, 0x31, 0x00, 0x00, 0xc4, 0x12,
+		}
+		_, _ = (*conn).Write(data)
+		_, _ = (*conn).Read(buf)
+		_, _ = (*conn).Write([]byte{})
+		_, _ = (*conn).Read(buf)
+		var dataList []byte
+		flag := false
+		for i := len(buf) - 1; i >= 0; i-- {
+			if buf[i] != 0x00 || flag {
+				flag = true
+				dataList = append(dataList, buf[i])
+			}
+		}
+		var j int
+		for i := 0; i < len(dataList); i++ {
+			if int(dataList[i]) == i {
+				j = i
+			}
+		}
+		temp := dataList[0:j]
+		pathBytes := &bytes.Buffer{}
+		for i := len(temp) - 1; i >= 0; i-- {
+			pathBytes.Write([]byte{dataList[i]})
+		}
 		res := &model.Result{
 			Host:   (*conn).RemoteAddr().String(),
 			Name:   "RMI",
 			Finger: fmt.Sprintf("%x", buf[0:7]),
+			Path:   pathBytes.String(),
 		}
 		ResultChan <- res
 		_ = (*conn).Close()
